5746e91dd6412652a93653eae0ca88f15b6f64ae
Lucas <lucas.ribeiro711@gmail.com>
2025-01-11 13:32:58 -0300

    :heavy_plus_sign: build: install eslint and configure it

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    rules: {
+      indent: [
+        'error',
+        2
+      ],
+      'linebreak-style': [
+        'error',
+        'unix'
+      ],
+      quotes: [
+        'error',
+        'single'
+      ],
+      '@typescript-eslint/no-empty-interface': 'off'
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
