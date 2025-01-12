1bf4151fb65c00df94c044e75a07856b5dc15a1d
Lucas <lucas.ribeiro711@gmail.com>
2025-01-11 23:14:41 -0300

    :heavy_plus_sign: build: install dotenv and add env variables

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from './server/server'
 
-server.listen(3000, () => console.log('Server running on port 3000'))
+server.listen(process.env.PORT || 3000, () =>
+  console.log(`Server running on port ${process.env.PORT || 3000}`)
+)
